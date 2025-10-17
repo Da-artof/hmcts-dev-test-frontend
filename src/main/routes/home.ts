df58997e55a4f925f6539868f31ef08db5b82b33
Da-artof <85499672+Da-artof@users.noreply.github.com>
2025-10-17 18:40:58 +0100

    Add search by ID and delete

--- src/main/routes/home.ts
+++ src/main/routes/home.ts
@@ -1,17 +1,36 @@
 import { Application } from 'express';
 import axios from 'axios';
 
+
 export default function (app: Application): void {
+  let ID = '';
   app.get('/', async (req, res) => {
     try {
-      // An example of connecting to the backend (a starting point)
-      const response = await axios.get('http://localhost:4000/cases/1');
-    
-      console.log(response.data);
+      res.render('home', {"example": ''})
+    } catch (error) {
+      console.error('Error making request:', error);
+      res.render('home', {"example": ''});
+    }
+  });
+
+  app.post('/', async (req, res) => {
+    ID = req.body.ID;
+    try {
+      const response = await axios.get('http://localhost:4000/cases/' + ID);
       res.render('home', { "example": response.data });
     } catch (error) {
       console.error('Error making request:', error);
       res.render('home', {});
+    }
+  });
+  
+  app.post('/delete', async (req, res) => {
+    try {
+      await axios.delete('http://localhost:4000/cases/' + ID);
+      res.redirect('/');
+    } catch (error) {
+      console.error('Error making request:', error);
+      res.redirect('/');
     }
   });
 }
